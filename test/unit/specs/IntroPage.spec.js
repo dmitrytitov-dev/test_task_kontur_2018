8318dec70599b58981e495f62e270e049f88322f
Dmitry Titov <dmitry.titov.dev@gmail.com>
2018-01-26 17:14:15 +0500

    Тесты для IntroPage.vue

--- test/unit/specs/IntroPage.spec.js
+++ test/unit/specs/IntroPage.spec.js
@@ -3,9 +3,18 @@ import IntroPage from '@/components/IntroPage';
 
 describe('IntroPage.vue', () => {
   it('should render correct contents', () => {
+    const moveToGamePageMocked = jest.fn();
+    const propsData = {moveToGamePage: moveToGamePageMocked};
     const Constructor = Vue.extend(IntroPage);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('h1').textContent)
-      .toEqual('Memory game');
+    const vm = new Constructor({propsData}).$mount();
+
+    expect(vm.$el.querySelector('h1').textContent).toEqual('Memory game');
+
+    const button = vm.$el.querySelector('button');
+    expect(button.textContent.trim()).toEqual('Начать игру');
+
+    expect(moveToGamePageMocked.mock.calls.length).toBe(0);
+    button.click();
+    expect(moveToGamePageMocked.mock.calls.length).toBe(1);
   });
 });
